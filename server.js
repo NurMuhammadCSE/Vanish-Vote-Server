a876beb99c42586e0ae98998121dfd2463cd9fbf
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2025-03-10 22:58:29 +0600

    Origin Modify

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://vanish-vote-client.vercel.app/"],  
+  origin: ["https://vanish-vote-client.vercel.app/"],  
   methods: ["GET", "POST", "PUT", "DELETE"],  // Specify allowed HTTP methods
   credentials: true  // Important if using cookies or sessions
 }));
